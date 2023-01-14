db1e564b8b53f1840170e5867b99ecaad6e1707d
anssho <78744460+anssho@users.noreply.github.com>
2023-01-14 22:33:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-stats = [0,0,0,0,0,0]
-totalStats = 0
-sum = 0
-  function rollDice() {
-    const dice = [...document.querySelectorAll(".side1 .die-list")];
-    dice.forEach(die => {
+const stats = [0,0,0,0,0,0]
+let totalStats = 0
+let sum = 0
+  
+const rollDice = () => {
+    document.querySelectorAll(".side1 .die-list").forEach(die => {
       toggleClasses(die);
       randomNumber = getRandomNumber(1, 6);
       sum += randomNumber
@@ -12,23 +12,23 @@ sum = 0
       totalStats += 1
       die.dataset.roll = randomNumber
     });
-
    
     for(let i=0; i < 6; i++) {
         let statPercent = Math.floor((stats[i]/totalStats)*100)
         document.getElementById('stats'+(i+1)).innerHTML = '<span style="background-color: #250214;padding-right: '+statPercent+'%; color: white;">'+(i+1)+': '+statPercent+' %, times '+stats[i]+'</span>'
     } 
+    
     var mode = stats.indexOf(Math.max(...stats))+1
     var avg = sum/totalStats
     document.getElementById('statsTotal').innerHTML ='<p style="color: white;"> total rolls '+totalStats+', mode: '+mode+', avg: '+avg.toFixed(2)+'</p>'
   }
   
-  function toggleClasses(die) {
+  const toggleClasses = (die) => {
     die.classList.toggle("odd-roll");
     die.classList.toggle("even-roll");
   }
   
-  function getRandomNumber(min, max) {
+  const getRandomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
